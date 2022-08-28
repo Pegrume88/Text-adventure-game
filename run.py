425dbc179ce84165b6772d36018765e2648a42b1
Pegrume88 <pegrume88@gmail.com>
2022-08-28 15:26:12 +0000

    create game function with links to introduction and scene 1

--- run.py
+++ run.py
@@ -1,3 +1,82 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Created by Rhys Pegrume
+
+import sys
+import os
+import time
+
+
+def game():
+    print("""       
+                ==================================
+                =________WINCHESTER_HOUSE________=
+                =____________MYSTERY_____________= 
+                ==================================
+        
+
+                                                \n""")
+    print(r"""\
+
+                              _,                _.
+                            (  `)            (`  ).
+                        .=( ` ,_ `)    .-``(      ).
+                        (.__.:-`-_.'   (.,,(.       '`.
+                                            `--`--`'`
+                                ____.........__H_
+                            __/%%%%|%%%%%%%|%%%%\
+                        _ ()/%%|:II:|II:::II|:II:|_ _ _
+                        |-(()|--|:II:|II:H:II|:II:|-|-|-|
+                        `'.'"^  ^` "^ "^|"|^'"' `^`-.^~'
+                        Sher^  
+
+
+                """)
+
+
+    print("To start game enter y for yes and n for no.")                                           
+    start = input("=> ")
+    if start == "y":
+        clr_terminal()
+        introduction()
+    if start == "n":
+        clr_terminal()
+        print("Maybe next time")
+       
+
+def clr_terminal():
+    """
+   Clear screen after each function is declared
+    """
+    os.system('clear')
+
+def introduction():
+    """
+    Intro to game with path to the first scene
+    """
+    print(".         ________________INTRODUCTION____________\n")
+    print("""   
+            Welcom to the Winchester House Mystery Game!
+            This is a text based mystery game. You will need to use text based 
+            answers to traverse through the game and solve the mystery.
+    """)
+    print("""
+           ========================================\n
+    """)
+    # add picture of house here
+
+    print("Are you ready to play y/n")
+    scene = input("=>")
+    if scene == "y":
+        clr_terminal()
+        scene1()
+        
+    if scene == "n":
+        clr_terminal()
+        game()
+        
+
+
+def scene1():
+    print("test")
+
+
+game()
