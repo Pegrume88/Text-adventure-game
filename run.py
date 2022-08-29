f899f6b903da211f25763ca4c04f222251c6a8ad
Pegrume88 <pegrume88@gmail.com>
2022-08-29 11:00:58 +0000

    created message and option to leave the car and enter the  house, a file for images was created for ascci art

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
-#Created by Rhys Pegrume
+
 
 import sys
 import os
 import time
 
 inventory = []
+
 
 def game():
     print("""       
@@ -15,7 +16,7 @@ def game():
         
 
                                                 \n""")
-    print(r"""\
+    print(r"""
 
                               _,                _.
                             (  `)            (`  ).
@@ -39,6 +40,7 @@ def game():
     if start == "n":
         clr_terminal()
         print("Maybe next time")
+
 
 def addToInventory(item):
     inventory.append(item)
@@ -112,6 +114,9 @@ def searchCar():
         leaveCar()  
 
 def gloveCompartment():
+    """
+    first location to pick up items
+    """
     print("You open the glove compartment and find...\n")
     print("Youn find a phone and torch..\n\n")
 
@@ -120,28 +125,82 @@ def gloveCompartment():
     item = input("=> ")
     if item == "y":
         clr_terminal()
-        addToInventory("phone" + "torch")
-        phone()
+        print("items added to your inventory")
+        addToInventory("phone" "torch")
         print(inventory)
-
-#create inventory add key items.
+        phone()
     if item == "n":
         clr_terminal()
         leaveCar()
        
-        
-
-    
-# accessing phone
+#accessing phone
 def phone():
     print("You pick up the phone and look intently at it.")
-    print("Hoping to jog any memory you have of the events that led you here.\n")
-    print("you go into you your phone and see you have no signal")
-    
+    print("""
+    Hoping to jog any memory
+    you have of the events that led you here.""")
+    print("you go into you your phone and see you have no signal.")
+    print("you notice you have two missed calls from James.")
+    print("and a message...\n\n")
+
+    print("Options....\n")
+    print("1 -Read message ")
+    print("2 - Leave car\n\n")
+    print("Select option 1 or 2 - \n")
+    option = input("=> ")
+    if option == "1":
+        clr_terminal()
+        print("Message from James")
+        print("""
+        Buddy you got to come check out this house. 
+        Its perfect for our next Pod Cast.
+        I will send you the address meet me at 
+        the Old Winchester House on friday,
+        you will not be dissapointed.
+        \n\n""")
+        print("""
+        'I remember now... I was meeting James at the location to
+        record an episode of our haunted house pod cast'...\n
+        'But how did i get here...'
+        """)
+        print("Still confused you peer through the windscreen of you car.")
+        print("""
+        Through the heave rain you can just about make out
+        a large house shrouded in darkness.
+        """)
+        leaveCar()
+
+    if option == "2":
+        clr_terminal()
+        print("Still confused you peer through the windscreen of you car.")
+        print("""
+        Through the heave rain you can just about make out
+        a large house shrouded in darkness.
+        """)
+        
+        leaveCar()
 
 def leaveCar():
     print("you you push the door open...")
-    print("lightning cracks above your head!")
+    print("lightning cracks above your head!\n")
+    print(" you sheild you face from the stinging rain,")
+    print(" and run to the house.\n\n")
 
+    print("Options....\n")
+    print("1 - open door")
+    print("2 - Look through the window\n\n")
+
+    print("Select Option enter 1 or 2")
+    option = input("=> ")
+    if option == "1":
+        clr_terminal()
+        enter_house()
+    elif option == "2":
+        clr_terminal()
+        window()    
+    
+def enter_house():
+
+def window():
 
 game()
